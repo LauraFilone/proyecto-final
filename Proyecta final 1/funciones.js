0b5429bfa34bbe0fb8fdd76c1985d8960cf5dcb7
LauraFilone <86857160+LauraFilone@users.noreply.github.com>
2023-03-08 23:16:42 -0300

    Add files via upload

---
+++ Proyecta final 1/funciones.js
@@ -0,0 +1,7 @@
+
+document.getElementById('boton').addEventListener('click', function() {
+    window.confirm("+54 011 31542654");
+});
+document.getElementById('boton_mail').addEventListener('click', function() {
+    window.confirm("cristinaperez@gmail.com");
+});
